664120b72506ea8f9ca96f663bb40df9e406a92b
Jackeagle <jackeagle102@gmail.com>
2016-07-03 20:04:18 +0530

    Revert "SystemUI: Add double tap to sleep gesture [1/2]"
    This reverts commit 0926c983e5497b5fe560a1f9e36576a6772cb662.
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3454,13 +3454,6 @@ public final class Settings {
 
 
         /**
-         *  Enable statusbar double tap gesture on to put device to sleep
-         * @hide
-         */
-        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
-
-
-        /**
          * Three Finger Gesture from Oppo
          *
          * @hide
