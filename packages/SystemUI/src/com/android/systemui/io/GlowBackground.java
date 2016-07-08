zephiK/android_frameworks_base

SystemUI: Lockscreen shortcut customization * Adopted for non-CM ROMs by zephiK
Change-Id: I4abf1deed0a342e111453516fa7820191a55ffc4

packages/SystemUI/src/com/android/systemui/cm/GlowBackground.java
@@ -1,0 +1,83 @@
+package com.android.systemui.cm;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.view.animation.AnticipateOvershootInterpolator;
+
+public class GlowBackground extends Drawable implements ValueAnimator.AnimatorUpdateListener {
+
+    private static final int MAX_CIRCLE_SIZE = 150;
+
+    private final Paint mPaint;
+    private Animator mAnimator;
+    private float mCircleSize;
+
+    public GlowBackground(int color) {
+        mPaint = new Paint();
+        mPaint.setColor(color);
+    }
+
+    private void startAnimation(boolean hide) {
+        if (mAnimator != null) {
+            mAnimator.cancel();
+        }
+        if (hide && mCircleSize == 0f) {
+            return;
+        } else if (!hide && mCircleSize == MAX_CIRCLE_SIZE) {
+            return;
+        }
+        mAnimator = getAnimator(hide);
+        mAnimator.start();
+    }
+
+    private Animator getAnimator(boolean hide) {
+        float start = mCircleSize;
+        float end = MAX_CIRCLE_SIZE;
+        if (hide) {
+            end = 0f;
+        }
+        ValueAnimator animator = ObjectAnimator.ofFloat(start, end);
+        animator.setInterpolator(new AnticipateOvershootInterpolator());
+        animator.setDuration(300);
+        animator.addUpdateListener(this);
+        return animator;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(getBounds().width() / 2, getBounds().height() / 2, mCircleSize, mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mCircleSize = (Float) valueAnimator.getAnimatedValue();
+        invalidateSelf();
+    }
+
+    public void hideGlow() {
+        startAnimation(true);
+    }
+
+    public void showGlow() {
+        startAnimation(false);
+    }
+}
+

packages/SystemUI/src/com/android/systemui/cm/LockscreenShortcutsHelper.java
@@ -1,0 +1,196 @@
+package com.android.systemui.cm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.Settings;
+import android.os.Handler;
+import android.os.Looper;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.systemui.R;
+
+public class LockscreenShortcutsHelper {
+
+    private Handler mHandler;
+
+    public enum Shortcuts {
+        LEFT_SHORTCUT(0),
+        RIGHT_SHORTCUT(1);
+
+        private final int index;
+
+        Shortcuts(int index) {
+            this.index = index;
+        }
+    }
+
+    public static final String DEFAULT = "default";
+    public static final String NONE = "none";
+    private static final String DELIMITER = "|";
+
+    private final Context mContext;
+    private OnChangeListener mListener;
+    private List<String> mTargetActivities;
+
+    public interface OnChangeListener {
+        void onChange();
+    }
+
+    public LockscreenShortcutsHelper(Context context, OnChangeListener listener) {
+        mContext = context;
+        if (listener != null) {
+            mListener = listener;
+            mHandler = new Handler(Looper.getMainLooper());
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_TARGETS), false, mObserver);
+        }
+        fetchTargets();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(null) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            fetchTargets();
+            if (mListener != null && mHandler != null) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mListener.onChange();
+                    }
+                });
+            }
+        }
+    };
+
+    public void cleanup() {
+        mContext.getContentResolver().unregisterContentObserver(mObserver);
+        mListener = null;
+    }
+
+    public static class TargetInfo {
+        public Drawable icon;
+        public ColorFilter colorFilter;
+        public String uri;
+        public TargetInfo(Drawable icon, ColorFilter colorFilter, String uri) {
+            this.icon = icon;
+            this.colorFilter = colorFilter;
+            this.uri = uri;
+        }
+    }
+
+    private void fetchTargets() {
+        mTargetActivities = Settings.Secure.getDelimitedStringAsList(mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_TARGETS, DELIMITER);
+        int itemsToPad = Shortcuts.values().length - mTargetActivities.size();
+        if (itemsToPad > 0) {
+            for (int i = 0; i < itemsToPad; i++) {
+                mTargetActivities.add(DEFAULT);
+            }
+        }
+    }
+
+    public Drawable getDrawableForTarget(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (intent != null) {
+            PackageManager pm = mContext.getPackageManager();
+            ActivityInfo info = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+            if (info != null) {
+                return getScaledDrawable(info.loadIcon(pm));
+            }
+        }
+        return null;
+    }
+
+    public String getUriForTarget(Shortcuts shortcuts) {
+        return mTargetActivities.get(shortcuts.index);
+    }
+
+    private Drawable getScaledDrawable(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            Resources res = mContext.getResources();
+            int width = res.getDimensionPixelSize(R.dimen.keyguard_affordance_icon_width);
+            int height = res.getDimensionPixelSize(R.dimen.keyguard_affordance_icon_height);
+            return new BitmapDrawable(mContext.getResources(),
+                    Bitmap.createScaledBitmap(((BitmapDrawable) drawable).getBitmap(),
+                            width, height, true));
+        } else {
+            return drawable;
+        }
+    }
+
+    private String getFriendlyActivityName(Intent intent, boolean labelOnly) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    private String getFriendlyShortcutName(Intent intent) {
+        String activityName = getFriendlyActivityName(intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public String getFriendlyNameForUri(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            return getFriendlyActivityName(intent, false);
+        }
+        return getFriendlyShortcutName(intent);
+    }
+
+    public boolean isTargetCustom(Shortcuts shortcut) {
+        if (mTargetActivities == null || mTargetActivities.isEmpty()) {
+            return false;
+        }
+        String action = mTargetActivities.get(shortcut.index);
+        if (DEFAULT.equals(action)) {
+            return false;
+        }
+
+        return NONE.equals(action) || getIntent(shortcut) != null;
+    }
+
+    public boolean isTargetEmpty(Shortcuts shortcut) {
+        return mTargetActivities != null &&
+                !mTargetActivities.isEmpty() &&
+                mTargetActivities.get(shortcut.index).equals(NONE);
+    }
+
+    public Intent getIntent(Shortcuts shortcut) {
+        Intent intent = null;
+        try {
+            intent = Intent.parseUri(mTargetActivities.get(shortcut.index), 0);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return intent;
+    }
+
+    public void saveTargets(ArrayList<String> targets) {
+        Settings.Secure.putListAsDelimitedString(mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_TARGETS, DELIMITER, targets);
+    }
+}

Jackeagle/io_base

Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java

packages/SystemUI/src/com/android/systemui/io/GlowBackground.java
@@ -1,0 +1,83 @@
+package com.android.systemui.io;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.view.animation.AnticipateOvershootInterpolator;
+
+public class GlowBackground extends Drawable implements ValueAnimator.AnimatorUpdateListener {
+
+    private static final int MAX_CIRCLE_SIZE = 150;
+
+    private final Paint mPaint;
+    private Animator mAnimator;
+    private float mCircleSize;
+
+    public GlowBackground(int color) {
+        mPaint = new Paint();
+        mPaint.setColor(color);
+    }
+
+    private void startAnimation(boolean hide) {
+        if (mAnimator != null) {
+            mAnimator.cancel();
+        }
+        if (hide && mCircleSize == 0f) {
+            return;
+        } else if (!hide && mCircleSize == MAX_CIRCLE_SIZE) {
+            return;
+        }
+        mAnimator = getAnimator(hide);
+        mAnimator.start();
+    }
+
+    private Animator getAnimator(boolean hide) {
+        float start = mCircleSize;
+        float end = MAX_CIRCLE_SIZE;
+        if (hide) {
+            end = 0f;
+        }
+        ValueAnimator animator = ObjectAnimator.ofFloat(start, end);
+        animator.setInterpolator(new AnticipateOvershootInterpolator());
+        animator.setDuration(300);
+        animator.addUpdateListener(this);
+        return animator;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(getBounds().width() / 2, getBounds().height() / 2, mCircleSize, mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        mCircleSize = (Float) valueAnimator.getAnimatedValue();
+        invalidateSelf();
+    }
+
+    public void hideGlow() {
+        startAnimation(true);
+    }
+
+    public void showGlow() {
+        startAnimation(false);
+    }
+}
+

packages/SystemUI/src/com/android/systemui/io/LockscreenShortcutsHelper.java
@@ -1,0 +1,196 @@
+package com.android.systemui.io;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.Settings;
+import android.os.Handler;
+import android.os.Looper;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.systemui.R;
+
+public class LockscreenShortcutsHelper {
+
+    private Handler mHandler;
+
+    public enum Shortcuts {
+        LEFT_SHORTCUT(0),
+        RIGHT_SHORTCUT(1);
+
+        private final int index;
+
+        Shortcuts(int index) {
+            this.index = index;
+        }
+    }
+
+    public static final String DEFAULT = "default";
+    public static final String NONE = "none";
+    private static final String DELIMITER = "|";
+
+    private final Context mContext;
+    private OnChangeListener mListener;
+    private List<String> mTargetActivities;
+
+    public interface OnChangeListener {
+        void onChange();
+    }
+
+    public LockscreenShortcutsHelper(Context context, OnChangeListener listener) {
+        mContext = context;
+        if (listener != null) {
+            mListener = listener;
+            mHandler = new Handler(Looper.getMainLooper());
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_TARGETS), false, mObserver);
+        }
+        fetchTargets();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(null) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            fetchTargets();
+            if (mListener != null && mHandler != null) {
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mListener.onChange();
+                    }
+                });
+            }
+        }
+    };
+
+    public void cleanup() {
+        mContext.getContentResolver().unregisterContentObserver(mObserver);
+        mListener = null;
+    }
+
+    public static class TargetInfo {
+        public Drawable icon;
+        public ColorFilter colorFilter;
+        public String uri;
+        public TargetInfo(Drawable icon, ColorFilter colorFilter, String uri) {
+            this.icon = icon;
+            this.colorFilter = colorFilter;
+            this.uri = uri;
+        }
+    }
+
+    private void fetchTargets() {
+        mTargetActivities = Settings.Secure.getDelimitedStringAsList(mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_TARGETS, DELIMITER);
+        int itemsToPad = Shortcuts.values().length - mTargetActivities.size();
+        if (itemsToPad > 0) {
+            for (int i = 0; i < itemsToPad; i++) {
+                mTargetActivities.add(DEFAULT);
+            }
+        }
+    }
+
+    public Drawable getDrawableForTarget(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (intent != null) {
+            PackageManager pm = mContext.getPackageManager();
+            ActivityInfo info = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+            if (info != null) {
+                return getScaledDrawable(info.loadIcon(pm));
+            }
+        }
+        return null;
+    }
+
+    public String getUriForTarget(Shortcuts shortcuts) {
+        return mTargetActivities.get(shortcuts.index);
+    }
+
+    private Drawable getScaledDrawable(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            Resources res = mContext.getResources();
+            int width = res.getDimensionPixelSize(R.dimen.keyguard_affordance_icon_width);
+            int height = res.getDimensionPixelSize(R.dimen.keyguard_affordance_icon_height);
+            return new BitmapDrawable(mContext.getResources(),
+                    Bitmap.createScaledBitmap(((BitmapDrawable) drawable).getBitmap(),
+                            width, height, true));
+        } else {
+            return drawable;
+        }
+    }
+
+    private String getFriendlyActivityName(Intent intent, boolean labelOnly) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    private String getFriendlyShortcutName(Intent intent) {
+        String activityName = getFriendlyActivityName(intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public String getFriendlyNameForUri(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            return getFriendlyActivityName(intent, false);
+        }
+        return getFriendlyShortcutName(intent);
+    }
+
+    public boolean isTargetCustom(Shortcuts shortcut) {
+        if (mTargetActivities == null || mTargetActivities.isEmpty()) {
+            return false;
+        }
+        String action = mTargetActivities.get(shortcut.index);
+        if (DEFAULT.equals(action)) {
+            return false;
+        }
+
+        return NONE.equals(action) || getIntent(shortcut) != null;
+    }
+
+    public boolean isTargetEmpty(Shortcuts shortcut) {
+        return mTargetActivities != null &&
+                !mTargetActivities.isEmpty() &&
+                mTargetActivities.get(shortcut.index).equals(NONE);
+    }
+
+    public Intent getIntent(Shortcuts shortcut) {
+        Intent intent = null;
+        try {
+            intent = Intent.parseUri(mTargetActivities.get(shortcut.index), 0);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return intent;
+    }
+
+    public void saveTargets(ArrayList<String> targets) {
+        Settings.Secure.putListAsDelimitedString(mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_TARGETS, DELIMITER, targets);
+    }
+}